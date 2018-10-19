8ede40e16bc08483e91b2f29b3d1b425c191253c
Jatin Kumar <jatin15011999@gmail.com>
2018-10-19 23:36:03 +0530

    extension v.0.0.2

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
   $.ajax({
     url: "https://elocals.in/assignment/pdtsearch.php?tok=rydnksjd854mfk52", 
@@ -7,33 +9,76 @@ $(document).ready(function(){
       let res = JSON.parse(res1);
       res.map((r)=>{
         console.log(r.images[0].url);
-        let width=100;
+        let width=180;
+        let title = r.titles.title;
+        let subtitle = r.titles.subtitle;
+        subtitle = subtitle === 'None' ? '' : subtitle;
+        let price = r.price;
         let imgSrc = r.images[0].url.replace(/{@width}/g, width);
         imgSrc = imgSrc.replace(/{@height}/g,width);
         imgSrc = imgSrc.replace(/q={@quality}/g,'');
-        $("#modal-content").append(
-            "<div class='content'>"+
-            r.titles.title+
-            "</div>"
-            // "<img src=" + imgSrc + "/>"
+        $("#items").append(
+            '<div class="item">'+
+            '<div class="item-name">' +
+            title +
+            '</div>' +
+            '<div class="item-details">' +
+              '<div class="img">'+
+              '<img src=' + imgSrc + '/>'+
+              '</div>' +
+              '<div class="details">' +
+                '<div class="title">' +
+                title +
+                '</div>' +
+                '<div class="subtitle">' +
+                subtitle +
+                '</div>' +
+                '<div class="price">' +
+                'Rs. ' + price +
+                '</div>' +
+              '</div>' +
+            '</div>' +
+            '<div>'
           )
       })
     },
     beforeSend: function(){
       $('#loader').show();
+      // $('.jotter').hide();
     },
     complete: function(){
         $('#loader').hide();
+        $('.jotter').css({display:'flex'})
+
+        // $('.jotter').show();
     },
   });
-  // $.get("https://elocals.in/assignment/pdtsearch.php?tok=rydnksjd854mfk52", function(data, status){
-  //   // let res1 = data.toString();
-  //   let res2 = data.replace(/'/g, '"')
-  //   let res1 = res2.replace(/None/g, '"None"')
-  //   let res = JSON.parse(res1);
-  //   res.map((r)=>{
-  //     console.log(r.titles.title);
-  //     $("#modal-content").append("<div class='content'>"+r.titles.title+"</div>")
-  //   })
-  // });
+
+  $("#items .item").click(function(){
+    alert('hjh');
+  })
+
+  $(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#items .item").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+
+  $('#left').click(function(){
+    $('#items').animate( { scrollLeft: '-=400' }, 200);
+  })
+
+  $('#right').click(function(){
+    $('#items').animate( { scrollLeft: '+=400' }, 200);
+  })
+
+  $('#items').hover(function(){
+    $('html,body').css({height:'320px'});
+  },function(){
+    $('html,body').css({height:'0px'});
+  })
+  
 });
